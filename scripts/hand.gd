e9a8e4833fbe85c13fba274e179c2cbdb601660d
louisng-coder <1402louis.nguyen@gmail.com>
2025-07-31 14:58:19 +0700

    added charging mechanic

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -8,11 +8,20 @@ extends RigidBody2D
 @onready var shoulder = get_parent().get_node("Body/shoulder")
 @onready var body = get_parent().get_node("Body")
 @onready var camera = get_parent().get_node("Body/Camera2D")
-
+@onready var loop_timer = get_parent().get_node("loop_timer")
 var shotgun = preload("res://scenes/shotgun.tscn")
 var shotgun_object = shotgun.instantiate()
 
 var touching_surface = false
+
+const OFFSET_MULTIPLIER = 400.0
+const CAMERA_LERP_SPEED = 4.0
+const MAX_FORCE := 90000.0      
+const CHARGE_RATE := 1000000.0    
+
+var charge_force := 0.0
+var is_charging := false
+
 
 func _ready():
 	call_deferred("_attach_shotgun", shotgun_object)
@@ -61,14 +70,57 @@ func _physics_process(delta: float) -> void:
 			GlobalVariables.to_target = to_target
 			GlobalVariables.spring_force = spring_force.length()
 			GlobalVariables.damping_force = damping_force.length()
+	
 
-	if Input.is_action_just_pressed("shoot"):
-		var recoil_strength = 30000.0
-		var gun_direction = shotgun_object.actual_direction
-		body.apply_impulse(-recoil_strength * gun_direction)
+# hold to charge the shotgun blast
+	if Input.is_action_pressed("shoot"):
+		is_charging = true
+		charge_force += CHARGE_RATE * delta
+		charge_force = clamp(charge_force, 0, MAX_FORCE)
+		Engine.time_scale = 0.03  # slow motion effect
+	elif Input.is_action_just_released("shoot") and is_charging:
+		is_charging = false
+		Engine.time_scale = 1.0  # reset time scale
+
+		var dir = shotgun_object.actual_direction.normalized()
+		body.apply_impulse(-dir * charge_force)
+		print(charge_force)
+		charge_force = 0.0
+
+		
+		
+		
+		
+#AAA
+	if Input.is_action_just_pressed("loop"):
+		if loop_timer.time_left > 0:
+			_perform_loop()
+	if loop_timer.time_left == 0:
+		_perform_loop()
+		
+
+
+	# 1. get mouse and player positions
+	var mouse_pos = get_global_mouse_position()
+	var player_pos = global_position
+
+	# 2. figure out how big the viewport is
+	var vp_size = get_viewport().get_visible_rect().size
+
+	# 3. compute an offset vector in screen-relative space
+	var offset_vec = (mouse_pos - player_pos) / (vp_size) * OFFSET_MULTIPLIER
+
+	# 4. lerp your Camera2D.offset toward that
+	camera.offset = camera.offset.lerp(-offset_vec, delta * CAMERA_LERP_SPEED)
 
 func _on_grab_area_body_entered(body: Node2D) -> void:
 	touching_surface = true
 
 func _on_grab_area_body_exited(body: Node2D) -> void:
 	touching_surface = false
+
+func _perform_loop():
+	pass
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
